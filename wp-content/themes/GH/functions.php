6709f5d810a53de9391dd4b0729df0208de38d39
kiliar <lpfankiliar@gmail.com>
2014-11-09 18:24:06 +0200

    Homework 3 update3

--- wp-content/themes/GH/functions.php
+++ wp-content/themes/GH/functions.php
@@ -5,5 +5,6 @@ function geekhub_setup() {
 	add_theme_support( 'post-thumbnails' );
 }
 add_action( 'after_setup_theme', 'geekhub_setup' );
-
+remove_filter( 'the_content', 'wpautop' );
+remove_filter( 'the_excerpt', 'wpautop' );
 ?>
--- wp-content/themes/GH/page.php
+++ wp-content/themes/GH/page.php
@@ -1,6 +1,7 @@
 <?php get_header(); ?>
 
 <div class="content-page">
+	
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post();
 		the_content();?>
 		<script type="text/javascript">
